6eb3ce598132beac44ff43f0fa8f4659e6673977
JofranDev <jofrandeveloper@gmail.com>
2024-07-08 11:56:25 -0300

    Resolvido la pantalla oscura

--- main.py
+++ main.py
@@ -10,15 +10,24 @@ pygame.display.set_caption("Mi primer juego con Pygame")
 running = True
 
 #variables de colores
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+#coordenadas y tamaño del rectangulo
+rect_x = 100
+rect_y = 100
+rect_width = 200
+rect_height = 100
 
 #loop principal del juego
 while running:
+
+	screen.fill("purple")
+	pygame.draw.rect(screen, BLUE, [rect_x, rect_y, rect_width, rect_height])
+	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	pygame.display.update()
 
 #actualizar pantalla
-screen.fill(WHITE)
 pygame.display.flip()
